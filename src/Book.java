979ae2005516e471be27d418e02d5ff1837d2605
User <essena23@gmail.com>
2024-03-24 17:38:06 -0400

    HW

---
+++ src/Book.java
@@ -0,0 +1,29 @@
+public class Book {
+    private String bookName;
+    private Author author;
+    private int yearPublish;
+
+
+    public Book(String bookName, Author author, int yearPublish) {
+        this.bookName = bookName;
+        this.author = author;
+        this.yearPublish = yearPublish;
+
+    }
+    public String getBookName() {
+        return this.bookName;
+    }
+
+    public Author getAuthor() {
+        return this.author;
+    }
+
+    public int getYearPublish() {
+        return this.yearPublish;
+    }
+
+    public void setYearPublish(int yearPublish) {
+        this.yearPublish = yearPublish;
+    }
+
+}
